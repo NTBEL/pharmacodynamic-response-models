8db635f9558a394d400394fb9e92e0f31bbb79e2
Blake A. Wilson <blakeaw1102@gmail.com>
2021-04-08 14:44:56 -0500

    Added a setup.py file for installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='pharmacodynamic',
+      version='0.1.0',
+      description='Collection of pharmacodynamic response models.',
+      author='Blake A. Wilson',
+      author_email='blakeaw1102@gmail.com',
+      url='https://github.com/NTBEL/pharmacodynamic-response-models',
+      packages=['pharmacodynamic'],
+      license='MIT',
+      keywords=['response model', 'dose-response', 'receptor-response']
+     )
